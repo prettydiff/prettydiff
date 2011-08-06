5bf6fc3acec680631412863a5dd0c9a25250802f
austincheney <austin.cheney@us.army.mil>
2011-08-06 07:37:44 -0600

    cross-domain XHR now supported via proxy.php

---
+++ proxy.php
@@ -0,0 +1,20 @@
+<?php
+$referer = $_SERVER['HTTP_REFERER'];
+$refpos = strpos($referer, 'prettydiff.com');
+if ($refpos === 7 || $refpos === 11) {
+    $x = $_GET['x'];
+    //$r = file_get_contents($x);
+    //echo($r);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $x);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    header("Content-type: text/plain;charset=UTF-8");
+    echo($result);
+} else {
+    header("HTTP/1.1 301 Moved Permanently");
+    header("Location: http://prettydiff.com/"); 
+}
+?>
