54cc7c373c4e2bdf9c8181ec1abb6f213c085166
austin cheney <edward.a.cheney.mil@mail.mil>
2015-11-02 10:18:38 -0700

    css quote convert enhancement

--- prettydiff.js
+++ prettydiff.js
@@ -1662,7 +1662,7 @@ global.edition        = {
     markuppretty : 151031, //markuppretty lib
     prettydiff   : 151029, //this file
     safeSort     : 151003, //safeSort lib
-    version      : "1.15.6", //version number
+    version      : "1.15.7", //version number
     webtool      : 151029
 };
 global.edition.latest = (function edition_latest() {
