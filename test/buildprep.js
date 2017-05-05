1d4b60eb5643596e7b52bcdd120e0521e340ae2f
prettydiff <info@prettydiff.com>
2017-05-05 08:58:30 -0500

    debugging travis build

--- test/buildprep.js
+++ test/buildprep.js
@@ -6,22 +6,27 @@
         a = 1;
     node.child("git clone https://github.com/prettydiff/biddle.git", function prep_clone(cer, stdcout, stdcer) {
         if (cer !== null) {
+            a = -10;
             return console.log(cer);
         }
         if (stdcer !== null && stdcer !== "") {
+            a = -10;
             return console.log(stdcer);
         }
         console.log("biddle clone complete!");
         node.child("node biddle global", {cwd:"biddle"}, function prep_clone_global(ger, stdgout, stdger) {
             if (ger !== null) {
+                a = -10;
                 return console.log(ger);
             }
             if (stdger !== null && stdger !== "") {
+                a = -10;
                 return console.log(stdger);
             }
             console.log("biddle global complete!");
             node.child("biddle install http://prettydiff.com/downloads/jslint/jslint_latest.zip", function prep_clone_global_install(ier, stdiout, stdier) {
                 var test = require("./lint.js");
+                a = -10;
                 if (ier !== null) {
                     return console.log(ier);
                 }
@@ -29,13 +34,12 @@
                     return console.log(stdier);
                 }
                 console.log("Pretty Diff test execute!");
-                a = -10;
                 test();
                 return [stdcout, stdgout, stdiout];
             });
         });
     });
     do {
-        a = a + 1;
+        setTimeout(console.log("Waiting..."), 1000);
     } while (a > 0);
 }());
