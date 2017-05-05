37502d0ce4b2ba85562fd124a930c57dee6b8534
prettydiff <info@prettydiff.com>
2017-05-05 09:12:33 -0500

    debugging travis build

--- test/buildprep.js
+++ test/buildprep.js
@@ -1,40 +1,42 @@
 (function prep() {
     "use strict";
-    var node = {
+    var node  = {
             child: require("child_process").exec
         },
-        a = false,
+        msg   = "",
         delay = function prep_delay() {
             setTimeout(function prep_delay_timeout() {
-                console.log("Waiting...");
-                if (a === false) {
+                if (msg === "") {
+                    console.log("Waiting...");
                     prep_delay();
+                } else {
+                    console.log(msg);
                 }
             }, 1000);
         };
     node.child("git clone https://github.com/prettydiff/biddle.git", function prep_clone(cer, stdcout, stdcer) {
         if (cer !== null) {
-            a = true;
+            msg = "git clone failed";
             return console.log(cer);
         }
         if (stdcer !== null && stdcer !== "") {
-            a = true;
+            msg = "git clone failed";
             return console.log(stdcer);
         }
         console.log("biddle clone complete!");
         node.child("node biddle global", {cwd:"biddle"}, function prep_clone_global(ger, stdgout, stdger) {
             if (ger !== null) {
-                a = true;
+                msg = "biddle global failed";
                 return console.log(ger);
             }
             if (stdger !== null && stdger !== "") {
-                a = true;
+                msg = "biddle global failed";
                 return console.log(stdger);
             }
             console.log("biddle global complete!");
             node.child("biddle install http://prettydiff.com/downloads/jslint/jslint_latest.zip", function prep_clone_global_install(ier, stdiout, stdier) {
                 var test = require("./lint.js");
-                a = true;
+                msg = "jslint installed by biddle";
                 if (ier !== null) {
                     return console.log(ier);
                 }
