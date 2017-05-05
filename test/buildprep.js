1bf46f26e860d865168c7bdbd25175671a820cdd
prettydiff <info@prettydiff.com>
2017-05-05 09:14:51 -0500

    debugging travis build

--- test/buildprep.js
+++ test/buildprep.js
@@ -16,33 +16,35 @@
         };
     node.child("git clone https://github.com/prettydiff/biddle.git", function prep_clone(cer, stdcout, stdcer) {
         if (cer !== null) {
-            msg = "git clone failed";
+            msg = cer;
             return console.log(cer);
         }
         if (stdcer !== null && stdcer !== "") {
-            msg = "git clone failed";
+            msg = stdcer;
             return console.log(stdcer);
         }
         console.log("biddle clone complete!");
         node.child("node biddle global", {cwd:"biddle"}, function prep_clone_global(ger, stdgout, stdger) {
             if (ger !== null) {
-                msg = "biddle global failed";
+                msg = ger;
                 return console.log(ger);
             }
             if (stdger !== null && stdger !== "") {
-                msg = "biddle global failed";
+                msg = stdger;
                 return console.log(stdger);
             }
             console.log("biddle global complete!");
             node.child("biddle install http://prettydiff.com/downloads/jslint/jslint_latest.zip", function prep_clone_global_install(ier, stdiout, stdier) {
                 var test = require("./lint.js");
-                msg = "jslint installed by biddle";
                 if (ier !== null) {
+                    msg = ier;
                     return console.log(ier);
                 }
                 if (stdier !== null && stdier !== "") {
+                    msg = stdier;
                     return console.log(stdier);
                 }
+                msg = "jslint installed by biddle";
                 console.log("Pretty Diff test execute!");
                 test();
                 return [stdcout, stdgout, stdiout];
